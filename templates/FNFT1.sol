f972483fda9fd19a3e4d480fc5e530661cf8a8cd
emrahm <e.mersinli@gmail.com>
2023-04-13 14:24:55 +0000

    First commit

---
+++ templates/FNFT1.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+ 
+
+contract #FNFT is ERC721PresetMinterPauserAutoId, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+    uint256 private _maxSupply = #MAXSUPPLY;
+    
+    constructor ()
+        ERC721PresetMinterPauserAutoId(
+          "#NAME",
+          "#TOKEN",
+          "#URI"
+        )
+    {}
+
+    function mint(address _to) public virtual override {
+      require(hasRole(MINTER_ROLE, _msgSender()), "Must have minter role to mint");
+      require(totalSupply() <= _maxSupply, "Max Supply Has Been Reached");
+      _mint(_to, _tokenIdCounter.current());
+      _tokenIdCounter.increment();
+    }
+
+    /**
+     *  Prevents people from accidentally sending funds here.
+     */
+    receive () external payable {
+      require(false, "This contract is for minting VL F-NFTs.  It does not allow for receiving payments.");
+    }
+}
