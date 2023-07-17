0d472b141a79edb26016c3c39d0b066aa7150c32
TokenFab Contract Creater <gitcommando@tokenfab.co>
2023-07-17 13:37:15 +0000

    ThorneIT0000014 are deployed

---
+++ contracts/ThorneIT0000014/ThorneIT0000014.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+ 
+
+contract ThorneIT0000014 is ERC721PresetMinterPauserAutoId, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+    uint256 private _maxSupply = 180;
+    
+    constructor ()
+        ERC721PresetMinterPauserAutoId(
+          "ENG-Bournemouth/ Poole-Dorset, E Block 302 (3rd Floor)",
+          "TS0014",
+          "https://marketplace.thornebilisim.com/ing-bournemouth-poole-dorset-e-block-302-3rd-floor"
+        )
+    {}
+
+    function mint(address _to) public virtual override {
+      require(hasRole(MINTER_ROLE, _msgSender()), "Must have minter role to mint");
+      require(totalSupply() <= _maxSupply, "Max Supply Has Been Reached");
+      _mint(_to, _tokenIdCounter.current());
+      _tokenIdCounter.increment();
+    }
+
+    /**
+     *  Prevents people from accidentally sending funds here.
+     */
+    receive () external payable {
+      require(false, "This contract is for minting NFTs.  It does not allow for receiving payments.");
+    }
+}
